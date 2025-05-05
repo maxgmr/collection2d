c3fc182d6e28314da630bbb21cbdbacf6c3faafd
Max Gilmour <mgilmour@maxgmr.ca>
2025-05-05 13:18:25 -0700

    Cargo init

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! `collection2d` provides functionality for two-dimensional collections.
+
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    clippy::all,
+    clippy::pedantic,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::todo,
+    clippy::dbg_macro
+)]
